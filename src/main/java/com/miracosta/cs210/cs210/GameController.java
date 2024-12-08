c17f297b9f8bfb00e2bd010345df37d56c27c2c7
csolomonson <ctsolomonson@gmail.com>
2024-12-07 19:52:37 -0800

    Add utilities to highlight a square

--- src/main/java/com/miracosta/cs210/cs210/GameController.java
+++ src/main/java/com/miracosta/cs210/cs210/GameController.java
@@ -1,9 +1,12 @@
 package com.miracosta.cs210.cs210;
 
 
+import com.miracosta.cs210.cs210.chess.pieces.ChessPiece;
 import com.miracosta.cs210.cs210.game.GameBoard;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
 
 public class GameController {
 
@@ -14,32 +17,43 @@ public class GameController {
     final double PIECE_SIZE = 50;
     public AnchorPane anchorPane;
     ImageManager images;
+    GameBoard board;
 
     public GameController() {
         images = ImageManager.getInstance();
 
     }
     public void initialize() {
-        GameBoard board = new GameBoard();
-        for (int i = 0; i < 64; i++) {
-            ImageView blackBishop = new ImageView(images.blackBishop);
-            renderPiece(blackBishop, i % 8, i / 8);
-        }
+        board = new GameBoard();
+        updateBoard();
     }
 
     public void handleUndoMove() {
         clearBoard();
+        removeHighlights();
     }
 
     public void handleRestartGame() {
-        images = ImageManager.getInstance();
+        highlightSquare(1,0);
+    }
+
+    public void drawBoard() {
+        for (ChessPiece piece : board.getChessPieces()) {
+            renderPiece(piece.getImage(), piece.getPosition().getRow(), piece.getPosition().getColumn());
+        }
     }
 
     public void clearBoard() {
         anchorPane.getChildren().removeIf(node -> node.getClass() == ImageView.class);
     }
 
-    private void renderPiece(ImageView pieceImage, int c, int r) {
+    public void updateBoard() {
+        clearBoard();
+        drawBoard();
+    }
+
+    private void renderPiece(Image piece, int r, int c) {
+        ImageView pieceImage = new ImageView(piece);
         pieceImage.setFitHeight(PIECE_SIZE);
         pieceImage.setFitWidth(PIECE_SIZE);
         pieceImage.setLayoutX(X_OFFSET + SQUARE_WIDTH / 2 - PIECE_SIZE / 2 + c * SQUARE_WIDTH);
@@ -53,5 +67,19 @@ public class GameController {
     public void handleExitToMenu() {
         SceneManager assets = SceneManager.getInstance();
         assets.stage.setScene(assets.mainMenu);
+    }
+
+    public void highlightSquare(int r, int c) {
+        Region rect = new Region();
+        rect.setPrefWidth(SQUARE_WIDTH);
+        rect.setPrefHeight(SQUARE_HEIGHT);
+        rect.setStyle("-fx-background-color: transparent; -fx-border-style: solid; -fx-border-width: 5; -fx-border-color: green;");
+        rect.setLayoutX(c*SQUARE_WIDTH + X_OFFSET);
+        rect.setLayoutY(r*SQUARE_HEIGHT + Y_OFFSET);
+        anchorPane.getChildren().add(rect);
+    }
+
+    public void removeHighlights() {
+        anchorPane.getChildren().removeIf(node -> node.getClass() == Region.class);
     }
 }
